05c0f6d5a8e9780567c1f756f822c9f7362b962b
olivia-e-a <olivia.avouris@gmail.com>
2024-03-12 12:31:30 +0000

    Added forecast functionality

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -44,10 +45,15 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
   }
 
-  function displayForecast() {
+  function getForecast(city) {
+    let apiKey = "1a747f2d7ac32a100bt13fab8776o6ca";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+  }
+
+  function displayForecast(response) {
     let days = [`Wed`, `Thu`, `Fri`, `Sat`, `Sun`];
     let forecastHtml = "";
-
 
     days.forEach(function (day) {
        forecastHtml = 
@@ -73,6 +79,6 @@ function handleSearchSubmit(event) {
   searchFormElement.addEventListener("submit", handleSearchSubmit);
 
   searchCity("Glasgow");
-  displayForecast();
+  
 
   
